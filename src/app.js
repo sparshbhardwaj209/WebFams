eb5202260f9da0877b7800e95feb593bf0404d8e
Sparsh Bhardwaj <sparshbhardwaj209@gmail.com>
2024-12-27 19:35:04 +0530

    playing with complex route regex

--- src/app.js
+++ src/app.js
@@ -7,25 +7,46 @@ app.get('/user', (req, res) => {
     res.send('Data GET request');
 })
 
-app.post('/user', (req, res)=> {
-    res.send('Data POST request');
-})
+// works with all the routes having a
+// app.get('/a', (req, res) => {
+//     res.send({name: 'Sparsh', age: 24});
+// })
 
-app.delete('/user', (req, res)=>{
-    res.send('Data Delete request');
-})
+// works with all the routes having abc or abbc or abbbc
+// app.get('/ab+c', (req, res) => {
+//     res.send({name: 'Sparsh', age: 24});
+// })
 
-app.patch('/user', (req, res)=>{
-    res.send("Data Patch request");
-})
+// works with all the routes having abd or ad (b is optional to include)
+// app.get('/ab?d', (req, res) => {
+//     res.send({name: 'Sparsh', age: 24});
+// })
 
-app.put('/user', (req, res)=>{
-    res.send('Data PUT request');
-})
 
-app.use('/test', (req, res) => {
-    res.send('hello sparsh ');
-})
+// works with all the routes having abcd or ad (bc is optional to include)
+// app.get('/a(bc)?d', (req, res) => {
+//     res.send({name: 'Sparsh', age: 24});
+// })
+
+// works with all the routes having abcd or abcbcd or abcbcbcd (bc can be multiple)
+// app.get('/a(bc)*d', (req, res) => {
+//     res.send({name: 'Sparsh', age: 24});
+// })
+
+// works with all the routes having abcd or absparshcd or abxyzcd (anything can be there in between ab and cd)
+// app.get('/ab*cd', (req, res) => {
+//     res.send({name: 'Sparsh', age: 24});
+// })
+
+// works with all the routes having a/ or a/b or a/c
+// app.get('/a/', (req, res) => {
+//     res.send({name: 'Sparsh', age: 24});
+// })
+
+// works with will the words ending with fly
+// app.get('/*fly$/', (req, res) => {
+//     res.send({name: 'Sparsh', age: 24});
+// })
 
 app.listen(PORT, () =>{
     console.log("Server is running on port 7777");
