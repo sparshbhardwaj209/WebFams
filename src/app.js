c294530994a61d71acb24431a556e6147f968c3b
Sparsh Bhardwaj <sparshbhardwaj209@gmail.com>
2024-12-31 18:36:26 +0530

    creating users by email and feed api's

--- src/app.js
+++ src/app.js
@@ -1,22 +1,49 @@
 const express = require("express");
 const connectDB = require("./config/database.js");
 const User = require("./models/user.js");
-require('dotenv').config()
+require("dotenv").config();
 const app = express();
 const PORT = process.env.PORT || 7777;
 
 app.use(express.json());
 
-app.post("/signup", async (req, res)=>{
-    // creating an instance of user model and getting the body data from the request
-    const user = new User(req.body);
-    try{
-        await user.save();
-        res.send("User created successfully");
-    }catch(err){
-        console.log("User not created", err.message);
+app.post("/signup", async (req, res) => {
+  // creating an instance of user model and getting the body data from the request
+  const user = new User(req.body);
+  try {
+    await user.save();
+    res.send("User created successfully");
+  } catch (err) {
+    console.log("User not created", err.message);
+  }
+});
+
+// get users by emailId
+app.get("/users", async (req, res) => {
+  try {
+    const userEmail = req.body.emailId;
+    const users = await User.find({ emailId: userEmail });
+    if (!users) {
+      res.status(404).send("User not found");
     }
-})
+    res.send(users);
+  } catch (err) {
+    res.status(500).send("Something went wrong, can't get the user!");
+  }
+});
+
+// Feed API
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+    if (!users) {
+      res.status(404).send("User not found");
+    }
+  } catch (err) {
+    res.status(500).send("Something went wrong, can't get the user!");
+  }
+});
 
 connectDB()
   .then(() => {
