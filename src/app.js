59c911d6893f37b496e3d51babdacb6014dbdaee
Sparsh Bhardwaj <sparshbhardwaj209@gmail.com>
2025-01-03 20:09:32 +0530

    schema validations

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ const PORT = process.env.PORT || 7777;
 
 app.use(express.json());
 
+// Signup API - to create a new user
 app.post("/signup", async (req, res) => {
   // creating an instance of user model and getting the body data from the request
   const user = new User(req.body);
@@ -14,7 +15,8 @@ app.post("/signup", async (req, res) => {
     await user.save();
     res.send("User created successfully");
   } catch (err) {
-    console.log("User not created", err.message);
+    // console.log("User not created" +  err);
+    res.status(400).send("User not created " + err.message);
   }
 });
 
@@ -32,7 +34,7 @@ app.get("/users", async (req, res) => {
   }
 });
 
-// Feed API
+// Feed API - to get all the users
 app.get("/feed", async (req, res) => {
   try {
     const users = await User.find({});
@@ -44,6 +46,35 @@ app.get("/feed", async (req, res) => {
     res.status(500).send("Something went wrong, can't get the user!");
   }
 });
+
+// delete user API using ID
+app.delete("/users", async (req, res)=>{
+  try{
+    const userId = req.body.userId;
+    const user = await User.findByIdAndDelete(userId);
+    if(!user){
+      res.status(404).send("User not found");
+    }
+    res.send("User deleted successfully");
+  }catch (err) {
+    res.status(500).send("Something went wrong, can't get the user!");
+  }
+})
+
+//update user API
+app.patch("/users", async (req, res)=>{
+  const userId = req.body.userId;
+  const data = req.body;
+  try{
+    await User.findByIdAndUpdate(userId, data, {
+      returnDocument: "after",
+      runValidators: true,
+    });
+    res.send("User updated successfully");
+  }catch (err) {
+    res.status(500).send("Updataion failed " + err.message );
+  }
+})
 
 connectDB()
   .then(() => {
--- src/models/user.js
+++ src/models/user.js
@@ -1,25 +1,55 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema(
+  {
     firstName: {
-        type: String
+      type: String,
+      required: true,
+      minLength: 3,
+      maxLength: 20,
     },
     lastName: {
-        type: String
+      type: String,
     },
     emailId: {
-        type: String
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
     },
     password: {
-        type: String
+      type: String,
+      minLength: 8,
+      required: true,
     },
     age: {
-        type: Number
+      type: Number,
+      min: 18,
     },
     gender: {
-        type: String
-    }
-})
+      type: String,
+      validate(value) {
+        if (!["male", "female", "others"].includes(value)) {
+          throw new Error("Gender can be 'male', 'female', or 'others' only");
+        }
+      },
+    },
+    photoUrl: {
+      type: String,
+      default:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSNi9kQ-9UyVu9-YrrtvRHuRckHgMtpIKwAuQ&s",
+    },
+    about: {
+      type: String,
+      default: "Hey there! I am using WebFams",
+    },
+    skills: {
+      type: [String],
+    },
+  },
+  { timestamps: true },
+);
 
 const User = mongoose.model("User", userSchema);
 module.exports = User;
